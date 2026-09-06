8dc18e3803d482964629951b586482629ecc9683
Bamodjak <60649401+bamodjak@users.noreply.github.com>
2026-09-06 16:26:02 +0100

    Rename server preferences to Flishly

--- app/src/main/java/com/filebridge/server/ServerConfig.kt
+++ app/src/main/java/com/filebridge/server/ServerConfig.kt
@@ -2,7 +2,7 @@ package com.filebridge.server
 
 object ServerConfig {
     const val PORT = 4444
-    const val PREFS = "filebridge_settings"
+    const val PREFS = "flishly_settings"
     const val HOST_PREF = "bind_host"
     var password: String = ""
 }
