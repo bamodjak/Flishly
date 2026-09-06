f0fdc76ab6aa445bb68f99af1bf15051986f363c
Bamodjak <60649401+bamodjak@users.noreply.github.com>
2026-09-06 16:37:41 +0100

    Remove old FileBridge package path

--- app/src/main/java/com/filebridge/server/ServerConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.filebridge.server
-
-object ServerConfig {
-    const val PORT = 4444
-    const val PREFS = "flishly_settings"
-    const val HOST_PREF = "bind_host"
-    var password: String = ""
-}
