3dd8505fe29c03465955447c27ec1906a18af21d
Bamodjak <60649401+bamodjak@users.noreply.github.com>
2026-09-06 16:37:11 +0100

    Rename package to Flishly

---
+++ app/src/main/java/com/flishly/server/ServerConfig.kt
@@ -0,0 +1,8 @@
+package com.flishly.server
+
+object ServerConfig {
+    const val PORT = 4444
+    const val PREFS = "flishly_settings"
+    const val HOST_PREF = "bind_host"
+    var password: String = ""
+}
